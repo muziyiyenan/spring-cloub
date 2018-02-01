d05d5c0e762c9253d053cb83da2c01c5c78c1746
muziyiyenan <lynacyh@163.com>
2018-02-01 10:16:45 +0800

    Update Application.java

--- springcloudstudy/eurekaserver/src/main/java/com/ctjsoft/eureka/server/Application.java
+++ springcloudstudy/eurekaserver/src/main/java/com/ctjsoft/eureka/server/Application.java
@@ -1,1 +1,18 @@
+package com.ctjsoft.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by lynac on 2018/1/31.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+    public static void main(String[] args) {
+        /*启动eureka服务*/
+        SpringApplication.run(Application.class,args);
+    }
+}
 
